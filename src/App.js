17753f5bc62fe67e395e6695e4ef3d8a4366a8c6
Ivan Yesid Mejia Medina <dacldeigoalejo@gmail.com>
2022-03-25 16:05:38 -0500

    Subida de repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card.js';
 
 function App() {
+
+
+
+  const jsonData= require('./Components/Quote.json'); 
+  
+  for (let property in jsonData.quotes){
+    const written =  jsonData.quotes[0].quote
+    console.log(written)
+    // for(let quotee in written){
+    //   console.log(quotee[1])
+    // }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+
+        <Card text="Hola soy una prueba" />
+        
+     </div>
+
+    
   );
 }
+
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import "./Card.css";
+
+ const Card = ({text}) => {
+     return(
+         <div className="card-text">
+             <p>{text}</p>
+             <button></button>
+
+        </div>
+     )
+ }
+
+
+ export default Card
